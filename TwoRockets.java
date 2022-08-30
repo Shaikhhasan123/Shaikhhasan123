a99a9c4aa3f02809d3ed18d8d106b982ba10ac81
Shaikhhasan123 <111064712+Shaikhhasan123@users.noreply.github.com>
2022-08-30 02:06:14 -0400

    Create TwoRockets.java

---
+++ TwoRockets.java
@@ -0,0 +1,20 @@
+public class TwoRockets {
+  public static void main (String [] args){
+    System.out.ptintln("   /\\      /\\  "); 
+    System.out.ptintln("  /  \\    /  \\ ");      
+    System.out.ptintln(" /    \\  /    \\ ");  
+    System.out.ptintln("+------+ +------+ ");
+    System.out.ptintln("|      | |      | ");     
+    System.out.ptintln("|      | |      | ");     
+    System.out.ptintln("+------+ +------+ ");                 
+    System.out.ptintln("|United| |United| ");                  
+    System.out.ptintln("|States| |States| ");                  
+    System.out.ptintln("+------+ +------+ ");                  
+    System.out.ptintln("|      | |      | ");     
+    System.out.ptintln("|      | |      | ");
+    System.out.ptintln("+------+ +------+ ");                  
+    System.out.ptintln("   /\\      /\\   ");      
+    System.out.ptintln("  /  \\    /  \\  ");       
+    System.out.ptintln(" /    \\  /    \\ ");
+  }
+}  
