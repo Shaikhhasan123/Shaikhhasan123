f2af8e2d19210dabf871280092a81d2004458f7c
Shaikhhasan123 <111064712+Shaikhhasan123@users.noreply.github.com>
2022-08-29 20:34:18 -0400

    Create Escaping.java

---
+++ Escaping.java
@@ -0,0 +1,14 @@
+/**
+*class: Escaping
+*version:1.0
+*author: Shaikh Hasan
+*written: August 29, 2022
+*description of this program:
+*/
+public class Escaping{
+ public static void main(String[] args){
+   System.out.println("//////////////////////");
+   System.out.println("||||| Shaikh Hasan |||||");
+   System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+ } 
+}
