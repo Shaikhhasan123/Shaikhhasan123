e433c7e1e953dd97ca0a563e888c29fecaf9fece
Shaikhhasan123 <111064712+Shaikhhasan123@users.noreply.github.com>
2022-08-30 02:12:19 -0400

    Update TwoRockets.java

--- TwoRockets.java
+++ TwoRockets.java
@@ -1,20 +1,27 @@
+/**
+ *class: TwoRockets
+ *version:1.0
+ *author: Shaikh Hasan
+ *written: August 29, 2022,
+ *description of this program: This program will generate an output of characters forward slashes, backward slashes and letters.
+ */
 public class TwoRockets {
-  public static void main (String [] args){
-    System.out.ptintln("   /\\      /\\  "); 
-    System.out.ptintln("  /  \\    /  \\ ");      
-    System.out.ptintln(" /    \\  /    \\ ");  
-    System.out.ptintln("+------+ +------+ ");
-    System.out.ptintln("|      | |      | ");     
-    System.out.ptintln("|      | |      | ");     
-    System.out.ptintln("+------+ +------+ ");                 
-    System.out.ptintln("|United| |United| ");                  
-    System.out.ptintln("|States| |States| ");                  
-    System.out.ptintln("+------+ +------+ ");                  
-    System.out.ptintln("|      | |      | ");     
-    System.out.ptintln("|      | |      | ");
-    System.out.ptintln("+------+ +------+ ");                  
-    System.out.ptintln("   /\\      /\\   ");      
-    System.out.ptintln("  /  \\    /  \\  ");       
-    System.out.ptintln(" /    \\  /    \\ ");
-  }
-}  
+    public static void main (String [] args){
+        System.out.println("   /\\      /\\  ");
+        System.out.println("  /  \\    /  \\ ");
+        System.out.println(" /    \\  /    \\ ");
+        System.out.println("+------+ +------+ ");
+        System.out.println("|      | |      | ");
+        System.out.println("|      | |      | ");
+        System.out.println("+------+ +------+ ");
+        System.out.println("|United| |United| ");
+        System.out.println("|States| |States| ");
+        System.out.println("+------+ +------+ ");
+        System.out.println("|      | |      | ");
+        System.out.println("|      | |      | ");
+        System.out.println("+------+ +------+ ");
+        System.out.println("   /\\      /\\   ");
+        System.out.println("  /  \\    /  \\  ");
+        System.out.println(" /    \\  /    \\ ");
+    }
+}
