2b74a67b57165ba90d2b49675900272ecab1b1f4
Shaikhhasan123 <111064712+Shaikhhasan123@users.noreply.github.com>
2022-09-15 16:42:02 -0400

    Update CircleArea.java

--- CircleArea.java
+++ CircleArea.java
@@ -1,3 +1,11 @@
+/**
+ * Class: Circle Area
+ * @author: Shaikh Hasan
+ * @version: 1.0
+ * Course: ITEC 2140
+ * Date: September 15, 2022
+ * Description: This program will calculate the Area of a circle and will not generate any answer if a negative number is inputed. 
+ */
 import java.util.Scanner;
 public class CircleArea {
     public static void main (String[] args){
