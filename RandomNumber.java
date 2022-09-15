8e3378a7131c08fcf59fb483af9bacf5797ac034
Shaikhhasan123 <111064712+Shaikhhasan123@users.noreply.github.com>
2022-09-15 17:09:07 -0400

    Update RandomNumber.java

--- RandomNumber.java
+++ RandomNumber.java
@@ -1,3 +1,11 @@
+/**
+ * Class: Random Number
+ * @author: Shaikh Hasan
+ * @version: 1.0
+ * Course: ITEC 2140
+ * Date: September 15, 2022
+ * Description: This program will let the user input a random number from 0-10 and let them know if they choose the wining number or not.
+ */
 import java.util.Scanner;
 public class RandomNumber {
     public static void main(String[] args){
