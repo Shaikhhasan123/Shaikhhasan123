fd3f9dd2ea704dda767d52ce7a9ec1615af8440f
Shaikhhasan123 <111064712+Shaikhhasan123@users.noreply.github.com>
2022-09-15 22:13:00 -0400

    Update DayOfTheWeek.java

--- DayOfTheWeek.java
+++ DayOfTheWeek.java
@@ -7,29 +7,30 @@
  * Description: This program will generate output with names of days of a week when inputting a number from 1-7
  */
 import java.util.Scanner;
-public class DayOfTheWeek {
 
-    public static void main(String[] args)
-    {
+public class Day {
+
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        System.out.print("Input number: ");
-        int day = in.nextInt();
+        System.out.print("Enter the day of the week: ");
+        int n = in.nextInt();
 
-        System.out.println(getDayName(day));
+        if (n == 1){
+            System.out.println("Week day");
+        } else if (n == 2){
+            System.out.println("Week day");
+        }else if (n == 3) {
+            System.out.println("Week day");
+        }else if (n == 4) {
+            System.out.println("Week day");
+        }else if (n == 5) {
+            System.out.println("Week day");
+        }else if (n == 6) {
+            System.out.println("Weekend");
+        }else if (n == 7) {
+            System.out.println("Weekend");
+        } else
+            System.out.println("Invalid week day");
     }
-    public static String getDayName(int day) {
-        String dayName = "";
-        switch (day) {
-            case 1: dayName = "Monday"; break;
-            case 2: dayName = "Tuesday"; break;
-            case 3: dayName = "Wednesday"; break;
-            case 4: dayName = "Thursday"; break;
-            case 5: dayName = "Friday"; break;
-            case 6: dayName = "Saturday"; break;
-            case 7: dayName = "Sunday"; break;
-            default:dayName = "Invalid week day";
-        }
 
-        return dayName;
-    }
 }
